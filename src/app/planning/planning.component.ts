d8c6be09fbf7d4967fa192ea31f8fb50fdc38d4b
Nikita <vclan@mail.ru>
2021-09-06 00:05:06 +0300

    modal window

--- src/app/planning/planning.component.ts
+++ src/app/planning/planning.component.ts
@@ -3,13 +3,26 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-planning',
   templateUrl: './planning.component.html',
-  styleUrls: ['./planning.component.scss']
+  styleUrls: ['./planning.component.scss'],
 })
 export class PlanningComponent implements OnInit {
+  tab1: boolean = true;
+  tab2: boolean = false;
+  modal: boolean = true;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  setTab1() {
+    this.tab1 = true;
+    this.tab2 = false;
+  }
+  setTab2() {
+    this.tab2 = true;
+    this.tab1 = false;
+  }
+  setModal() {
+    this.modal = false;
   }
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
